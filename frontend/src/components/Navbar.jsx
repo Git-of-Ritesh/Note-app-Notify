3d738d94b24a442bac8054207d6df37eff31c3c2
Git-of-Ritesh <rajpandey3942@gmail.com>
2025-01-21 23:48:16 +0530

    first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../assets/logo/logo.png'
+import Searchbar from './searchbar/searchbar'
+
+
+const Navbar = () => {
+  return (
+    <nav className="flex bg-white text-black p-3">
+
+      <div className="flex items-center">
+        <img src={logo} alt="Logo" className="h-16 w-auto" />
+        <span className="-ml-1 text-lg font-bold font-Logo">Notetify</span>
+      </div>
+
+      {/*search bar*/}
+      <Searchbar/>
+
+    </nav>
+  )
+}
+
+export default Navbar
