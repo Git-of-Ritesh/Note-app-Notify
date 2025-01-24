6b675b1897eea5b227fab6f594d769dc18a2f772
Git-of-Ritesh <rajpandey3942@gmail.com>
2025-01-24 21:54:39 +0530

    made new notes botton responsive

--- frontend/src/components/Notecard/Notecard.jsx
+++ frontend/src/components/Notecard/Notecard.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Notecard = ({title, content, date, isPinned}) => {
     return (
         <div>
-            <div className='flex justify-between flex-col bg-my-grey w-full h-36 rounded-3xl p-3 mt-3'>
+            <div className='flex justify-between flex-col bg-my-grey w-full h-36 rounded-3xl p-4 mt-3'>
                 <h1 className='font-bold'>{title}</h1>
                 <p className='text-sm opacity-65'>{content?.slice(0, 150)}</p>
-                <h3 className='text-sm font-semibold'>{date}</h3>
+                <h3 className='text-sm font-semibold opacity-80'>{date}</h3>
             </div>
         </div>
     )
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import Navbar from '../../components/Navbar'
 import Notespage from '../../components/Notespage/Notespage'
 import CreateNote from '../../components/Createnotes/CreateNote'
 
 const Home = () => {
+  const [isCreateOpen, setIsCreateOpen] = useState(false);
+
   return (
     <div className='flex flex-col h-screen'>
       <Navbar />
@@ -14,11 +16,11 @@ const Home = () => {
         </div>
 
         <div>
-          <Notespage />
+          <Notespage onNewNote={() => setIsCreateOpen(true)} />
         </div>
 
-        <div>
-          <CreateNote />
+        <div className={`w-full transition-all ${isCreateOpen ? 'block' : 'hidden'}`}>
+          <CreateNote onClose={() => setIsCreateOpen(false) }/>
         </div>
       </div>
     </div>
