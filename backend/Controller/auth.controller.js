0296cad7aa69538286611d2709844b220e2536b8
Git-of-Ritesh <rajpandey3942@gmail.com>
2025-01-30 14:35:22 +0530

    implement the singout function

--- backend/Controller/auth.controller.js
+++ backend/Controller/auth.controller.js
@@ -1,15 +1,15 @@
 import User from '../Models/user.model.js';
 import bcryptjs from 'bcryptjs';
-import { errorHndler }  from '../utils/error.js';
-import jwt from 'jsonwebtoken'; 
+import { errorHndler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
-    
-    const isValidUser = await User.findOne({email});
 
-    if(isValidUser){
-        return next(errorHndler(400, 'User already exists'));   
+    const isValidUser = await User.findOne({ email });
+
+    if (isValidUser) {
+        return next(errorHndler(400, 'User already exists'));
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -34,28 +34,40 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
 
-    try{
-       const validUser = await User.findOne({email});
+    try {
+        const validUser = await User.findOne({ email });
 
-       if(!validUser){
-        return next(errorHndler(404, 'User not found'));
-       }
+        if (!validUser) {
+            return next(errorHndler(404, 'User not found'));
+        }
 
-       const isPasswordValid = bcryptjs.compareSync(password, validUser.password);
+        const isPasswordValid = bcryptjs.compareSync(password, validUser.password);
 
-       if(!isPasswordValid){
-        return next(errorHndler(401, 'Invalid credentials'));
-       }
-       const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        if (!isPasswordValid) {
+            return next(errorHndler(401, 'Invalid credentials'));
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
 
-       const {password: pass, ...rest} = validUser._doc;
+        const { password: pass, ...rest } = validUser._doc;
 
-       res. cookie("access_token", token, {httpOnly: true,}).status(200).json({sucess: true,
-        message: "logged in successfully",
-        rest,
-       });
-    }catch(error){
+        res.cookie("access_token", token, { httpOnly: true, }).status(200).json({
+            sucess: true,
+            message: "logged in successfully",
+            rest,
+        });
+    } catch (error) {
         next(error);
     }
+}
 
+export const signout = async (req, res, next) => {
+    try {
+        res.clearCookie("access_token").status(200).json({
+            success: true,
+            message: "Logged out successfully"
+        });
+
+    } catch (error) {
+        next(error);
+    }
 }
