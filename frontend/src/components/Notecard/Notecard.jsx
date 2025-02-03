bc67caef5bc49932501fb8b0f8d88e79a6c15b86
Git-of-Ritesh <rajpandey3942@gmail.com>
2025-02-03 16:27:16 +0530

    just formated date using moment.js

--- frontend/src/components/Notecard/Notecard.jsx
+++ frontend/src/components/Notecard/Notecard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import moment from 'moment'
 
 const Notecard = ({title, content, date}) => {
     return (
@@ -6,7 +7,7 @@ const Notecard = ({title, content, date}) => {
             <div className='flex justify-between flex-col bg-white w-full h-36 rounded-3xl p-4 mt-3'>
                 <h1 className='font-bold'>{title}</h1>
                 <p className='text-sm/4 opacity-65'>{content?.slice(0, 150)}</p>
-                <h3 className='text-sm font-semibold opacity-80'>{date}</h3>
+                <h3 className='text-sm font-semibold opacity-80'>{moment(date).format('L')}</h3>
             </div>
         </div>
     )
