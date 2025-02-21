0490b28dd3f3970e9600428a132dcca58e344d04
Git-of-Ritesh <rajpandey3942@gmail.com>
2025-02-21 22:59:00 +0530

    changes made in backend

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: ["http://localhost:5173"], credentials: true}));
+app.use(cors({origin: [process.env.FRONTEND_URL, "http://localhost:5173"], credentials: true}));
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
